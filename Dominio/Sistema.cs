61187805cf10596308b8d9c03a8d89d3c0aad036
Martin <martinsuarezalfonso07@gmail.com>
2025-04-08 11:19:11 -0300

    Metodo agregar admin

--- Dominio/Sistema.cs
+++ Dominio/Sistema.cs
@@ -17,9 +17,17 @@ namespace Dominio
         private List<Ruta> _rutas;
         private List<Vuelo> _vuelos;
 
+        public List<Administrador> Administrador { get { return new List<Administrador>(_administradores); } }
+        public List<Cliente> Cliente { get { return new List<Cliente>(_clientes); } }
+        public List<Aeropuerto> Aeropuerto { get { return new List<Aeropuerto>(_aeropuertos); } }
+        public List<Avion> Avion { get { return new List<Avion>(_aviones); } }
 
+        public List<Pasaje> Pasaje { get { return new List<Pasaje>(_pasajes); } }
+        public List<Ruta> Ruta { get { return new List<Ruta>(_rutas); } }
+        public List<Vuelo> Vuelo { get { return new List<Vuelo>(_vuelos); } }
 
-         private void PrecargarDatos()
+       
+        private void PrecargarDatos()
         {
             PrecargarClientes();
             PrecargarAdmin();
@@ -31,7 +39,6 @@ namespace Dominio
 
         private void PrecargarClientes()
         {
-
 
         }
         private void PrecargarAdmin()
@@ -52,7 +59,7 @@ namespace Dominio
 
         public void AgregarCliente(Cliente cliente)
         {
-            if(cliente == null)
+            if (cliente == null)
             {
                 throw new Exception("No puede ser nulo");
             }
@@ -62,6 +69,20 @@ namespace Dominio
                 throw new Exception("El cliente ya existe");
             }
             _clientes.Add(cliente);
+        }
+
+        public void AgregarAdmin(Administrador admin)
+        {
+            if( admin == null)
+            {
+                throw new Exception("No puede ser nulo");
+            }
+            admin.Validar();
+            if (_administradores.Contains(admin))
+            {
+                throw new Exception("El admin ya existe");
+            }
+            _administradores.Add(admin);
         }
     }
 }
